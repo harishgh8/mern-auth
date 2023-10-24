69c88f77855f760484e0b9ca2b8b31568f0a4078
harish <harishg.hiriyur@gmail.com>
2023-10-24 13:09:56 -0400

    mongoDB data base is connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongoDB");
+  })
+  .catch(() => {
+    console.log("err");
+  });
 const app = express();
 
 app.listen(3000, () => {
-  console.log("server says hellow and listening");
+  console.log("server says hello and listening");
 });
