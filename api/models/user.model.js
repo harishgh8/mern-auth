a22c533f9ece1ff383c08275b76c1edb71c82652
harish <harishg.hiriyur@gmail.com>
2023-10-24 13:19:49 -0400

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+// create a schema
+
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// create a model
+
+const User = mongoose.model("User", userSchema);
+export default User;
