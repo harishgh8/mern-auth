4374eab42b4ea5685d1a9a94bc0ec1b9561a15e7
harish <harishg.hiriyur@gmail.com>
2023-10-31 09:25:43 -0400

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 
+// connect to mongoDB
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -12,7 +16,14 @@ mongoose
     console.log("err");
   });
 const app = express();
+// in order to receive json as input we need this
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server says hello and listening");
 });
+
+// path or api routes
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    userName: {
+    username: {
       type: String,
       required: true,
       unique: true,
